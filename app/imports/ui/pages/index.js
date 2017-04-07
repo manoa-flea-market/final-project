f306fcb1fb0e9372466f9173a59c051a1afbf6d8
erikhuan <huangerik@hotmail.com>
2017-04-06 16:26:51 -1000

    Added pages

--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -8,3 +8,21 @@ import './home-page.html';
 
 import './list-stuff-page.html';
 import './list-stuff-page.js';
+
+import './listing-page.html';
+import './listing-page.js';
+
+import './add-listing-page.html';
+import './add-listing-page.js';
+
+import './edit-listing-page.html';
+import './edit-listing-page.js';
+
+import './edit-profile-page.html';
+import './edit-profile-page.js';
+
+import './market-page.html';
+import './market-page.js';
+
+import './profile-page.html';
+import './profile-page.js';
