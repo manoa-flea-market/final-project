d303ee735eb13a304c852e0d0fd765d52259cb25
rnaka <raena6@hawaii.edu>
2017-04-19 17:46:31 -1000

    created items and category collection

---
+++ app/imports/api/category/CategoryCollection.js
@@ -0,0 +1,119 @@
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import BaseCollection from '/imports/api/base/BaseCollection';
+import { check } from 'meteor/check';
+import { Meteor } from 'meteor/meteor';
+import { _ } from 'meteor/underscore';
+
+/** @module Category */
+
+/**
+ * Represents a specific category, such as "Textbook".
+ * @extends module:Base~BaseCollection
+ */
+class CategoryCollection extends BaseCollection {
+
+  /**
+   * Creates the Category collection.
+   */
+  constructor() {
+    super('Category', new SimpleSchema({
+      name: { type: String },
+      description: { type: String, optional: true },
+    }));
+  }
+
+  /**
+   * Defines a new Category.
+   * @example
+   * Categories.define({ name: 'Textbooks',
+   *                    description: 'UH Textbooks needed for classes at UH Manoa' });
+   * @param { Object } description Object with keys name and description.
+   * Name must be previously undefined. Description is optional.
+   * Creates a "slug" for this name and stores it in the slug field.
+   * @throws {Meteor.Error} If the category definition includes a defined name.
+   * @returns The newly created docID.
+   */
+  define({ name, description }) {
+    check(name, String);
+    check(description, String);
+    if (this.find({ name }).count() > 0) {
+      throw new Meteor.Error(`${name} is previously defined in another Category`);
+    }
+    return this._collection.insert({ name, description });
+  }
+
+  /**
+   * Returns the Category name corresponding to the passed interest docID.
+   * @param categoryID A category docID.
+   * @returns { String } A category name.
+   * @throws { Meteor.Error} If the category docID cannot be found.
+   */
+  findName(categoryID) {
+    this.assertDefined(categoryID);
+    return this.findDoc(categoryID).name;
+  }
+
+  /**
+   * Returns a list of Category names corresponding to the passed list of Category docIDs.
+   * @param categoryIDs A list of Category docIDs.
+   * @returns { Array }
+   * @throws { Meteor.Error} If any of the instanceIDs cannot be found.
+   */
+  findNames(categoryIDs) {
+    return categoryIDs.map(interestID => this.findName(categoryID));
+  }
+
+  /**
+   * Throws an error if the passed name is not a defined Category name.
+   * @param name The name of a category.
+   */
+  assertName(name) {
+    this.findDoc(name);
+  }
+
+  /**
+   * Throws an error if the passed list of names are not all Category names.
+   * @param names An array of (hopefully) Category names.
+   */
+  assertNames(names) {
+    _.each(names, name => this.assertName(name));
+  }
+
+  /**
+   * Returns the docID associated with the passed Category name, or throws an error if it cannot be found.
+   * @param { String } name A Category name.
+   * @returns { String } The docID associated with the name.
+   * @throws { Meteor.Error } If name is not associated with a Category.
+   */
+  findID(name) {
+    return (this.findDoc(name)._id);
+  }
+
+  /**
+   * Returns the docIDs associated with the array of Category names, or throws an error if any name cannot be found.
+   * If nothing is passed, then an empty array is returned.
+   * @param { String[] } names An array of category names.
+   * @returns { String[] } The docIDs associated with the names.
+   * @throws { Meteor.Error } If any instance is not a Category name.
+   */
+  findIDs(names) {
+    return (names) ? names.map((instance) => this.findID(instance)) : [];
+  }
+
+  /**
+   * Returns an object representing the Category docID in a format acceptable to define().
+   * @param docID The docID of a Category.
+   * @returns { Object } An object representing the definition of docID.
+   */
+  dumpOne(docID) {
+    const doc = this.findDoc(docID);
+    const name = doc.name;
+    const description = doc.description;
+    return { name, description };
+  }
+}
+
+/**
+ * Provides the singleton instance of this class to all other entities.
+ */
+export const Categories = new CategoryCollection();
---
+++ app/imports/api/items/ItemCollection.js
@@ -0,0 +1,76 @@
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import BaseCollection from '/imports/api/base/BaseCollection';
+import { Category } from '/imports/api/category/CategoryCollection';
+import { check } from 'meteor/check';
+import { Meteor } from 'meteor/meteor';
+
+/** @module Item */
+
+/**
+ * Items provide item data for a user.
+ * @extends module:Base~BaseCollection
+ */
+class ItemCollection extends BaseCollection {
+
+  /**
+   * Creates the Profile collection.
+   */
+  constructor() {
+    super('Item', new SimpleSchema({
+      name: { type: String },
+      price: { type: String },
+      category: {type: [String] },
+      // Remainder are optional
+      description: { type: String, optional: true },
+      picture: { type: SimpleSchema.RegEx.Url, optional: true },
+    }));
+  }
+
+  /**
+   * Defines a new Item.
+   * @example
+   * Items.define({ ({ name: 'Discrete Mathematics and it's Application',
+   *                   price: '49.50',
+   *                   category: ['textbook', 'school supplies'});
+   *                   description: 'Textbook used with ICS 141 and 241.'
+   *                   picture: www.google.com
+   * @param { Object } description Object with required key name, price, and category.
+   * Remaining keys are optional.
+   * Username must be unique for all users. It should be the UH email account.
+   * Interests is an array of defined interest names.
+   * @throws { Meteor.Error } If a user with the supplied username already exists, or
+   * if one or more interests are not defined, or if github, facebook, and instagram are not URLs.
+   * @returns The newly created docID.
+   */
+  define({ firstName = '', lastName = '', username, bio = '', interests, picture = '', title = '', github = '',
+      facebook = '', instagram = '' }) {
+    // make sure required fields are OK.
+    const checkPattern = { firstName: String, lastName: String, username: String, bio: String, picture: String,
+      title: String };
+    check({ firstName, lastName, username, bio, picture, title }, checkPattern);
+
+    if (this.find({ username }).count() > 0) {
+      throw new Meteor.Error(`${username} is previously defined in another Profile`);
+    }
+
+    // Throw an error if any of the passed Interest names are not defined.
+    Interests.assertNames(interests);
+    return this._collection.insert({ firstName, lastName, username, bio, interests, picture, title, github,
+      facebook, instagram });
+  }
+
+  /**
+   * Returns an object representing the Profile docID in a format acceptable to define().
+   * @param docID The docID of a Profile.
+   * @returns { Object } An object representing the definition of docID.
+   */
+  dumpOne(docID) {
+    const doc = this.findDoc(docID);
+    const name = doc.name;
+    const price = doc.price;
+    const category = doc.category;
+    const description = doc.description;
+    const picture = doc.picture;
+    return { name, price, category, description, picture };
+  }
+}
