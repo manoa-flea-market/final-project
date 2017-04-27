f034ad1bedb80431059cb3aa527dcd0955ab124c
erikhuan <huangerik@hotmail.com>
2017-04-27 10:58:25 -1000

    update

--- app/server/main.js
+++ app/server/main.js
@@ -1,8 +1,8 @@
 import '/imports/startup/server';
 import '/imports/startup/both';
 import '/imports/api/base';
-import '/imports/api/category';
 import '/imports/api/contacts';
-import '/imports/api/interest';
-import '/imports/api/items';
 import '/imports/api/profile';
+import '/imports/api/interest';
+import '/imports/api/category';
+import '/imports/api/items';
